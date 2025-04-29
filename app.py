c8ff6e878bc8b0836ee0e6850397a28dcee6031b
Xandar8668 <xandarhsu@gmail.com>
2025-04-29 16:04:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # app.py
-print("Hello from app.py!")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "✅ LINE Bot Flask Server is running on Vercel!"
+
+# 這段是 Vercel 要求的 entry point（確保有變數 app）
+# 不需要寫 app.run()，Vercel 自動處理啟動
