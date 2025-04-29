30c4e26e5daaddbd774c5ea3d01faab62abb39c5
Xandar8668 <xandarhsu@gmail.com>
2025-04-29 13:41:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 # config.py
-print("Hello from config.py!")
+
+# LINE Bot 金鑰
+CHANNEL_SECRET = "32b66b04ab78671cee9818afe6e1181c"
+CHANNEL_ACCESS_TOKEN = "H/ZkozFSaba/Y+elLuaOsFKQUgGminzMs/UC41L/RObnAAZs8dtJeSfNrbOrQqmzeoiJA7VTTm4ScBra714tJn29IxexgZth42cOGSIudku5xRTr3dKwBGDqAnOcdH0yD2rOZvveIpmHFQhXisQxdQdB04t89/1O/w1cDnyilFU="
+
+# 管理者 LINE 使用者 ID（限定可以查詢所有報名資料的人）
+ADMIN_USER_ID = "xandat999"
+
+# Google Sheets 活動清單試算表 ID
+EVENTS_SPREADSHEET_ID = "1p2TbBLsHXFV3gOJD5fGt50ZKLlciRsP_3ry44PD-ibc"
+
+# Google API 服務帳戶 JSON 憑證檔案名稱
+GOOGLE_CREDENTIAL_FILE = "credentials.json"
